bcfea127f77b5e18fd12e10627de2a9a089addcd
CYS-physics <219toto@snu.ac.kr>
2021-11-30 00:03:53 +0900

    new density

--- RTP_simul.py
+++ RTP_simul.py
@@ -667,13 +667,13 @@ def simul_scan(f_init, f_fin, N, N_ptcl):
         
 def L_scan_moments(fin,ffin,N,L):
     
-    direc ='211129_1/'
+    direc ='211130/'
 #     rho=10
-    rho=0.2
+    rho=0.5
 
 
     L=L
-    N_ptcl = 10*L
+    N_ptcl = 70*L
     a=0.9
     direc+='a/'+str(a)+'/L/'+str(L)+'/'
     os.makedirs(os.getcwd()+'/data/'+direc,exist_ok=True)
