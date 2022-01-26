869710387cacbdd20b36756b6f6584a9ba6b3629
CYS-physics <219toto@snu.ac.kr>
2022-01-26 22:17:40 +0900

    newer

--- RTP_simul.py
+++ RTP_simul.py
@@ -910,7 +910,7 @@ def f_density(N_ptcl, f_init, f_fin, N,name):
     
     
 def anomalous(f,duration, N_ptcl,progress = False):
-    date = '220125/'+str(N_ptcl)+'/'
+    date = '220126/'+str(N_ptcl)+'/'
     os.makedirs('image/anomalous/'+date,exist_ok=True)  
 #     os.makedirs('image/v_hist/'+date,exist_ok=True)  
 
