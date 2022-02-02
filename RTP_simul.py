ac90d1c5ca9ebde529d43907623d0dd0633f49ea
CYS-physics <219toto@snu.ac.kr>
2022-02-02 20:33:56 +0900

    new setting

--- RTP_simul.py
+++ RTP_simul.py
@@ -675,14 +675,14 @@ def simul_scan(f_init, f_fin, N, N_ptcl):
         
 def L_scan_moments(f,L):
     
-    direc ='220202_1/'
+    direc ='220202_2/'
 #     rho=10
-    rho=20
+    rho=5
 
 
     L=L
     N_ptcl = 5*L
-    a=1.0
+    a=1.1
     direc+='a/'+str(a)+'/L/'+str(L)+'/'
     os.makedirs(os.getcwd()+'/data/'+direc,exist_ok=True)
     
