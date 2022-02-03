a42654d6a95657a6c6b0a73419696ed34b5cb0ff
CYS-physics <219toto@snu.ac.kr>
2022-02-03 10:53:30 +0900

    new setting

--- RTP_simul.py
+++ RTP_simul.py
@@ -675,9 +675,9 @@ def simul_scan(f_init, f_fin, N, N_ptcl):
         
 def L_scan_moments(f,L):
     
-    direc ='220202_8/'
+    direc ='220202_9/'
 #     rho=10
-    rho=10
+    rho=40
 
 
     L=L
