86276c485d03628a2b8707314ed5805e09e61ad6
CYS-physics <219toto@snu.ac.kr>
2022-02-03 12:07:31 +0900

    more particles

--- RTP_simul.py
+++ RTP_simul.py
@@ -675,13 +675,13 @@ def simul_scan(f_init, f_fin, N, N_ptcl):
         
 def L_scan_moments(f,L):
     
-    direc ='220202_11/'
+    direc ='220203_1/'
 #     rho=10
     rho=10
 
 
     L=L
-    N_ptcl = 20*L
+    N_ptcl = 50*L
     a=0.9
     direc+='a/'+str(a)+'/L/'+str(L)+'/'
     os.makedirs(os.getcwd()+'/data/'+direc,exist_ok=True)
