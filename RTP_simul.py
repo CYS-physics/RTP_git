9715eb0073e0674605cab76f1075b19606c236ac
CYS-physics <219toto@snu.ac.kr>
2022-02-04 14:59:38 +0900

    longer L

--- RTP_simul.py
+++ RTP_simul.py
@@ -676,13 +676,13 @@ def simul_scan(f_init, f_fin, N, N_ptcl):
         
 def L_scan_moments(f,L):
     
-    direc ='220204_7/'
+    direc ='220204_8/'
 #     rho=10
     rho=1
 
 
     L=L
-    N_ptcl = 200*L
+    N_ptcl = 10*L
     a=0.9
     direc+='a/'+str(a)+'/L/'+str(L)+'/'
     os.makedirs(os.getcwd()+'/data/'+direc,exist_ok=True)
