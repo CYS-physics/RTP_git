28f0490ecd0057fa45c9b87aa3f9766047c97cfa
CYS-physics <219toto@snu.ac.kr>
2022-07-02 17:01:09 +0900

    larger ensemble

--- RTP_simul.py
+++ RTP_simul.py
@@ -1024,7 +1024,7 @@ def ageing(f,t_in,t_dur,N_ptcl,progress = False):
     
     
 def anomalous(f,t_dur, N_ptcl,progress = False):
-    date = '220702/N='+str(N_ptcl)+'/t_dur='+str(t_dur)+'/'
+    date = '220702_1/N='+str(N_ptcl)+'/t_dur='+str(t_dur)+'/'
     os.makedirs('image/anomalous/'+date,exist_ok=True) 
     os.makedirs('data/anomalous/'+date,exist_ok=True)
 #     os.makedirs('image/v_hist/'+date,exist_ok=True)  
@@ -1036,7 +1036,7 @@ def anomalous(f,t_dur, N_ptcl,progress = False):
     a = 1.1 # fc = 
     Fs=5000
     
-    RTP = RTP_lab(alpha=1, u=10, len_time=100, N_time=Fs,N_X=200, N_ptcl=N_ptcl, v=0, mu=1, muw = 1)
+    RTP = RTP_lab(alpha=1, u=10, len_time=100, N_time=Fs,N_X=2000, N_ptcl=N_ptcl, v=0, mu=1, muw = 1)
     RTP.compute = False
     RTP.l = 30
     RTP.L = 300
